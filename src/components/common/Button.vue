97ef7758a2a6e3054d2fedc5dbdb033d59028332
IbRuslan <ribragimov2003@gmail.com>
2023-12-17 22:01:51 +0500

    complete footer

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-  <button :class="buttonClasses" class="bg-[#5367CE] py-2 px-7 text-white rounded-md text-lg cursor-pointer hover:bg-[#455DDD] active:bg-[#3350E8]" @click="handleClick">
+  <button :class="buttonClasses" class="text-center bg-[#5367CE] py-2 px-7 text-white rounded-md text-lg cursor-pointer hover:bg-[#455DDD] active:bg-[#3350E8]" @click="handleClick">
     <slot></slot>
   </button>
 </template>
