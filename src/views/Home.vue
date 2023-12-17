c98aff829ca7dbe3ac9e0747a4179d065f8a6b0c
IbRuslan <ribragimov2003@gmail.com>
2023-12-18 00:16:09 +0500

    complete main block

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <script setup>
-import SkillCard from "@/components/modules/SkillCard.vue";
-import ReviewCard from "@/components/modules/ReviewCard.vue";
+import Button from "@/components/common/Button.vue";
 </script>
 
 <template>
-  <div>
-    <SkillCard images="/src/assets/images/icons/shipping.svg"
-               title="Морские перевозки"
-               description="Мы предлагаем морские грузоперевозки из страны и в страну"
-    />
-    <SkillCard images="/src/assets/images/icons/shipping.svg"
-               title="Морские перевозки"
-               description="Мы предлагаем морские грузоперевозки из страны и в страну"
-    />
-    <ReviewCard images="/src/assets/images/icons/clients.svg" :score="1250" title="Довольных клиентов" />
+  <div class="w-full bg-blend-darken h-screen bg-no-repeat"
+       style="background-image: url('/src/assets/images/background/delivered-directly-to-her-door.png')">
+    <div class="overlay flex justify-start items-center">
+      <div class="text-white ml-64 flex flex-col gap-6 w-2/5">
+        <div>
+          <p class="text-sm">Доставка груза: без границ и ограничений</p>
+        </div>
+        <div class="mb-24 flex flex-col gap-5">
+          <p class="text-6xl"><b>CARGO PRO - ваш ключ к простой и быстрой логистике.</b></p>
+          <p class="text-lg">Доверьте нам свои отправления, и мы гарантируем их<br/> своевременную и надежную доставку. Все ваши потребности -
+            наш<br/> приоритет!</p>
+        </div>
+        <div><Button>Наши сервисы</Button></div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<style>
+.overlay {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 768px;
+  background-color: rgb(176, 174, 174);
+  mix-blend-mode: multiply;
+}
+</style>
