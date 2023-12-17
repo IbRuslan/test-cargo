5e8c4ac6812877e53ee5d91a59d7383cecf0db17
IbRuslan <ribragimov2003@gmail.com>
2023-12-17 22:24:58 +0500

    complete skillcard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,16 @@
+<script setup>
+import SkillCard from "@/components/modules/SkillCard.vue";
+</script>
+
 <template>
-<div>Home</div>
+  <div>
+    <SkillCard images="/src/assets/images/icons/shipping.svg"
+               title="Морские перевозки"
+               description="Мы предлагаем морские грузоперевозки из страны и в страну"
+    />
+    <SkillCard images="/src/assets/images/icons/shipping.svg"
+               title="Морские перевозки"
+               description="Мы предлагаем морские грузоперевозки из страны и в страну"
+    />
+  </div>
 </template>
